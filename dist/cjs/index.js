1b94e0f5d049b08e4d55e2a1afcb94e2f3e680dc
immortalmice <128365559+emu-rabbit@users.noreply.github.com>
2023-03-21 11:17:19 +0800

    Chore: Env setup

---
+++ dist/cjs/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greeting = void 0;
+function greeting() {
+    console.log('greeting');
+}
+exports.greeting = greeting;
---
+++ dist/cjs/types/index.d.ts
@@ -0,0 +1,2 @@
+export declare function greeting(): void;
+//# sourceMappingURL=index.d.ts.map
