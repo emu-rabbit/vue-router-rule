460f2972477b1852fcfdb66d617f9f8c1b5396b9
絵夢羽さ沂 <128365559+emu-rabbit@users.noreply.github.com>
2023-03-25 10:38:59 +0800

    Chore: Playground setup

---
+++ playground/src/Layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div
+        class="grid grid-cols-5 w-screen"
+    >
+        <div
+            class="col-span-3 h-screen"
+        >
+
+        </div>
+        <div
+            class="col-span-2 h-screen"
+        >
+            <RouterView />
+        </div>
+    </div>
+</template>
